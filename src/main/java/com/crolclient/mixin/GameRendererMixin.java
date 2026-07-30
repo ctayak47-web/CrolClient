001ba7a8d78d8d1e77a1809648b4bdb39244100f
ctayak47-web <ctayak47@gmail.com>
2026-07-30 04:05:49 +0400

    Update GameRendererMixin.java

--- src/main/java/com/crolclient/mixin/GameRendererMixin.java
+++ src/main/java/com/crolclient/mixin/GameRendererMixin.java
@@ -1,6 +1,7 @@
 package com.crolclient.mixin;
 
 import net.minecraft.client.render.GameRenderer;
+import net.minecraft.client.render.RenderTickCounter;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -9,7 +10,10 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(GameRenderer.class)
 public class GameRendererMixin {
     @Inject(method = "render", at = @At("HEAD"))
-    private void onRender(float tickDelta, long startTime, boolean tick, CallbackInfo ci) {
+    private void onRender(RenderTickCounter tickCounter, boolean tick, CallbackInfo ci) {
+        // Если внутри нужен tickDelta, его можно достать вот так:
+        // float tickDelta = tickCounter.getTickDelta(true);
+        
         // Reserved
     }
 }
