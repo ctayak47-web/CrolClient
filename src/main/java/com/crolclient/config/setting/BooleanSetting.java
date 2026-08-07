f0f80760fb1e1130afe24921e2e32fcf0f4d2c61
ctayak47-web <ctayak47@gmail.com>
2026-08-08 01:28:38 +0400

    delete all files

--- src/main/java/com/crolclient/config/setting/BooleanSetting.java
+++
@@ -1,6 +0,0 @@
-package com.crolclient.config.setting;
-public class BooleanSetting extends Setting<Boolean> {
-    public BooleanSetting(String name, Boolean defaultValue) {
-        super(name, defaultValue);
-    }
-}
